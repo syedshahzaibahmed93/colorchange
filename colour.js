8057536dcbcadcbd25ac4dcb62c33ac18668a447
syedshahzaibahmed93 <133108199+syedshahzaibahmed93@users.noreply.github.com>
2023-06-10 03:20:54 +0500

    Add files via upload

---
+++ colour.js
@@ -0,0 +1,32 @@
+function  red(){
+    document.body.style.backgroundColor="red";
+    console.write(document.body.style.backgroundColor);
+
+};
+function  green(){
+    document.body.style.backgroundColor="green";
+    console.write(document.body.style.backgroundColor);
+
+};
+    
+function  blue(){
+    document.body.style.backgroundColor="blue";
+    console.write(document.body.style.backgroundColor);
+};
+    
+    function  yellow(){
+        document.body.style.backgroundColor="yellow";
+        console.write(document.body.style.backgroundColor);
+    
+    };
+// 
+        // function showcolor(){
+// let color=document.getElementById("colorinput").Value;
+// document.body.style.backgroundColor="color";
+// console.write(document.body.style.backgroundColor);
+// }
+    
+
+    
+        
+    
